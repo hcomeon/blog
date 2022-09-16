4b6ff98eb638b3ebe5e32364a6e503ff2671142f
Faithsky <11381828+Faithsky@user.noreply.gitee.com>
2022-09-16 17:44:55 +0800

    api接口封装

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,38 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <el-button @click="login">登录</el-button>
+    <div>用户名: {{ username }}</div>
   </div>
 </template>
 
-<script>
+<script language="javascript">
+import request from "./helpers/request";
+import auth from "@/api/auth";
+import blog from "@/api/blog";
+window.auth = auth;
+window.blog = blog;
+
 export default {
-  name: 'App'
-}
+  name: "App",
+  data() {
+    return {
+      username: "",
+    };
+  },
+  methods: {
+    login() {
+      auth.login({ username: "hunger", password: "123456" }).then((res) => {
+        console.log(res);
+        this.username = res.data.username;
+      });
+    },
+  },
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
